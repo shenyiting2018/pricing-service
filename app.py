3e0cc540ce17fb3f36a3a609b7ac8cc021113abd
Yiting <shenyiting2018@gmail.com>
2019-09-04 19:13:17 -0700

    Chapter 5

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from views.alerts import alert_blueprint
 app = Flask(__name__)
 
 app.register_blueprint(item_blueprint, url_prefix="/items")
+app.register_blueprint(alert_blueprint, url_prefix="/alerts")
 
 if __name__ == "__main__":
     app.run(debug=True)
